bbf8001a86430a67f0f7e52a444dd9524a35eef9
Khoa Le Quang Minh <lequangminhkhoa@gmail.com>
2026-06-02 22:23:30 +0700

    ignore seaborn

--- Hope Implementation/test_gmm.py
+++ Hope Implementation/test_gmm.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns # type: ignore
 from tqdm import tqdm
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split

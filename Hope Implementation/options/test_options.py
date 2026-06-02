537c115ff7312ae8437d418d7700d492caac6950
Khoa Le Quang Minh <lequangminhkhoa@gmail.com>
2026-06-02 16:39:04 +0700

    Add missing epoch_count parameter to test_options.py

--- Hope Implementation/options/test_options.py
+++ Hope Implementation/options/test_options.py
@@ -6,5 +6,6 @@ class TestOptions(BaseOptions):
         parser = BaseOptions.initialize(self, parser)
         parser.add_argument('--load_dir', type=str, default='/data/chwang/Log/ordinal',
                             help='models are loaded from here')
+        parser.add_argument('--epoch_count', type=int, default=60, help='which epoch checkpoint to load')
         self.isTrain = False
         return parser

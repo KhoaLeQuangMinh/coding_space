000d952a3711e2821a4ab6e8653692eec53177cb
Khoa Le Quang Minh <lequangminhkhoa@gmail.com>
2026-06-30 12:40:13 +0700

    new exp

--- Hope Implementation/utils/train_data.py
+++ Hope Implementation/utils/train_data.py
@@ -157,6 +157,10 @@ def train_data(model, total_cn_loader, total_ad_loader, total_mci_loader,
                 loss_hyb = (triplet_ins2cls_global / features.shape[1]) + (intra_pole_loss_dist / features.shape[1]) + (pole_sep_loss / features.shape[1])
             elif ablation_loss == 'triplet_pole_intra_dist_sep':
                 loss_hyb = (triplet_ins2cls / features.shape[1]) + (intra_pole_loss_dist / features.shape[1]) + (pole_sep_loss / features.shape[1])
+            elif ablation_loss == 'triplet_only_sep':
+                loss_hyb = (triplet_ins2cls / features.shape[1]) + (pole_sep_loss / features.shape[1])
+            elif ablation_loss == 'triplet_only_global_sep':
+                loss_hyb = (triplet_ins2cls_global / features.shape[1]) + (pole_sep_loss / features.shape[1])
             elif ablation_loss == 'exp_hierarchical_triplet_ins2cls':
                 loss_hyb = loss_ins2ins + (hierarchical_triplet_ins2cls / features.shape[1]) + loss_cls2cls
             elif ablation_loss == 'exp_3pole_local':
@@ -199,7 +203,7 @@ def train_data(model, total_cn_loader, total_ad_loader, total_mci_loader,
             if ablation_loss == 'triplet_only_sigreg' and sigreg_module is not None:
                 train_loss_sigreg += sigreg_loss.item()
                 
-            if ablation_loss in ['exp_triplet_ins2cls', 'triplet_only', 'triplet_only_collinear', 'triplet_only_sigreg', 'triplet_pole_intra', 'triplet_pole_intra_dist', 'triplet_only_global', 'triplet_pole_intra_dist_global', 'triplet_pole_intra_dist_global_sep', 'triplet_pole_intra_dist_sep']:
+            if ablation_loss in ['exp_triplet_ins2cls', 'triplet_only', 'triplet_only_collinear', 'triplet_only_sigreg', 'triplet_pole_intra', 'triplet_pole_intra_dist', 'triplet_only_global', 'triplet_pole_intra_dist_global', 'triplet_pole_intra_dist_global_sep', 'triplet_pole_intra_dist_sep', 'triplet_only_sep', 'triplet_only_global_sep']:
                 if ablation_loss == 'triplet_pole_intra':
                     train_loss_ins2cls += ((triplet_ins2cls + intra_pole_loss) / features.shape[1]).item()
                 elif ablation_loss == 'triplet_pole_intra_dist':
@@ -210,6 +214,10 @@ def train_data(model, total_cn_loader, total_ad_loader, total_mci_loader,
                     train_loss_ins2cls += ((triplet_ins2cls_global + intra_pole_loss_dist + pole_sep_loss) / features.shape[1]).item()
                 elif ablation_loss == 'triplet_pole_intra_dist_sep':
                     train_loss_ins2cls += ((triplet_ins2cls + intra_pole_loss_dist + pole_sep_loss) / features.shape[1]).item()
+                elif ablation_loss == 'triplet_only_sep':
+                    train_loss_ins2cls += ((triplet_ins2cls + pole_sep_loss) / features.shape[1]).item()
+                elif ablation_loss == 'triplet_only_global_sep':
+                    train_loss_ins2cls += ((triplet_ins2cls_global + pole_sep_loss) / features.shape[1]).item()
                 elif ablation_loss == 'triplet_only_global':
                     train_loss_ins2cls += (triplet_ins2cls_global / features.shape[1]).item()
                 else:
